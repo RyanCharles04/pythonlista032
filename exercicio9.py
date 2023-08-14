90de02ffcc6adbb883cb4ec3e3ef300cdec2f26b
Ryan Charles <ryancharles040808@gmail.com>
2023-08-14 09:58:50 -0300

    Reformulando a pergunta da questão 9

--- exercicio9.py
+++ exercicio9.py
@@ -4,8 +4,8 @@ em dias. Obs: Considere os anos com 365 dias e os meses com 30 dias.
 '''
 
 anos = int(input("Quantos anos você tem?"))
-meses = int(input("Quantos meses você tem?"))
-dias = int(input("Quantos dias você tem?"))
+meses = int(input("Quantos meses já se passaram de seu último aniversário?"))
+dias = int(input("Quantos dias se passaram após os meses relatados anteriormente?"))
 
 
 resultado = (anos*365)+(meses*30)+dias
